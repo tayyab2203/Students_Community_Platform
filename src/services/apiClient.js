5dbad0a02b9de2d328cb0ec3f42f3f73748c718c
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-10-20 22:06:39 +0500

    Change API base URL to production server

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -36,7 +36,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://studentscommunity.revivercmsolutions.com/api',
 });
 
 export default apiClient;
