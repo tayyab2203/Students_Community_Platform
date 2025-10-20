a934062ad057c06657c48c661c696c74622d127f
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-10-20 20:03:01 +0500

    Update Register path to production URL

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
   const navItems = [
   { name: "Home", path: "/" },
   { name: "About", path: "/about" },
-  { name: "Register", path: "http://localhost:8000/register-student" }, // 🔹 Laravel Blade page
+  { name: "Register", path: "https://studentscommunity.revivercmsolutions.com/register-student" }, // 🔹 Laravel Blade page
 ];
 
 
