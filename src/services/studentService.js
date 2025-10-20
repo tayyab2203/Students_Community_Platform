41f7fae5a27aa876be18e532852d38334e6913c7
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-10-20 10:35:46 +0500

    Uncomment student service functions and exports

--- src/services/studentService.js
+++ src/services/studentService.js
@@ -1,33 +1,19 @@
-// import apiClient from './apiClient';
-
-// // Named exports
-// export const getStudents = async () => {
-//   const response = await apiClient.get('/students');
-//   return response.data;
-// };
-
-// export const getStudent = async (id) => {
-//   const response = await apiClient.get(`/students/${id}`);
-//   return response.data;
-// };
-
-// // Default export (object form)
-// const studentService = { getStudents, getStudent };
-// export default studentService;
-
 import apiClient from './apiClient';
 
 // Named exports
 export const getStudents = async () => {
-  const response = await apiClient.get('/students');  // Base list
+  const response = await apiClient.get('/students');
   return response.data;
 };
 
 export const getStudent = async (id) => {
-  const response = await apiClient.get(`/students/${id}`);  // Detail by ID
+  const response = await apiClient.get(`/students/${id}`);
   return response.data;
 };
 
 // Default export (object form)
 const studentService = { getStudents, getStudent };
 export default studentService;
+
+
+
